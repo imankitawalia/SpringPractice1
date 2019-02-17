2ca99e955949c2f1c17c858db89a3306692decc6
imankitawalia <imankitawalia@gmail.com>
2019-02-18 01:21:34 +0530

    Understanding application.properties file.

--- src/main/java/com/example/demo1/controller/RestControllers.java
+++ src/main/java/com/example/demo1/controller/RestControllers.java
@@ -1,5 +1,6 @@
 package com.example.demo1.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,5 +16,16 @@ public class RestControllers {
     @RequestMapping(method = RequestMethod.POST, value = "/register/course")
     public String saveCourse(@RequestBody Course course){
       return "Your course name : "+course.getName()+ " with chapter count : "+course.getChapterCount()+" is saved.";
+    }
+
+    //To pick up default values from properties file
+    @Value("${default.course.name}")
+    private String cName;
+    @Value("${default.course.chapterCount}")
+    private int cChapterCount;
+    @RequestMapping("/defaultCourse")
+    public Course getDefaultEndPoint(@RequestParam(value = "name",defaultValue = "Spring Boot Course",required = false) String name,
+                              @RequestParam(value = "chapterCount", defaultValue = "3",required = false)int chapterCount){
+        return new Course(cName,cChapterCount);
     }
 }
